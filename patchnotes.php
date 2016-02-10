dca3b805e8f7e706574c06c9a34eef867d7fed48
Jacob Piatt <piattjacob@gmail.com>
2016-02-10 17:15:40 -0500

    Fixed login

--- patchnotes.php
+++ patchnotes.php
@@ -1,16 +1,21 @@
 <?php
-include "checklogin.php";
+	include "checklogin.php";
 ?>
-<h2>Coming Soon (Hopefully)</h2>
+<h2>Planned Updates</h2>
 <ul>
-	<li>Update hero database with maxhp/hp/maxmp/mp/initiative</li>
 	<li>Make an enemy editor form</li>
 	<li>Make a room generator form</li>
 	<li>Make a room editor form</li>
+</ul>
+<hr width='33%'>
+<h2>Known Bugs</h2>
+<ul>
+	<li>Hero list only shows heroes that are in a party</li>
+	<li>Hero database has 0's for maxhp/hp/maxmp/mp/initiative</li>
 	<li>Right now, race/prof attribute %'s are all over the places and static. Should consolidate them so we can update them if we need to balance later</li>
 	<li>Fix hero battleplan</li>
 	<li>Having an 'int' column is messing everything up. change to intel?</li>
-	<li>Fix items</li>
+	<li>Fix items. Then market</li>
 </ul>
 <hr width='33%'>
 <h2>February 10, 2016</h2>
@@ -19,14 +24,12 @@ include "checklogin.php";
 	<li>Started crying 'cause I have to redo the entire database :(</li>
 	<li>Continues crying 'cause I didn't realize I messed up halfway through redoing it all :(</li>
 </ul>
-<hr width='33%'>
 <h2>February 9, 2016</h2>
 <ul>
 	<li>Fixed the turn order to read initiative from database</li>
 	<li>***FIXED THE PARTY FROM REGAINING MAXHP EVERY ROOM***</li>
 	<li>Made an enemy generator form</li>
 </ul>
-<hr width='33%'>
 <h2>February 7, 2016</h2>
 <ul>
 	<li>Put maxhp/hp/maxmp/mp/initiative in enemy database</li>
@@ -34,7 +37,6 @@ include "checklogin.php";
 	<li>Updated enemy list with maxhp/maxmp/initiative</li>
 	<li>Updated enemy database with maxhp/hp/maxmp/mp/initiative</li>
 </ul>
-<hr width='33%'>
 <h2>February 5, 2016</h2>
 <ul>
 	<li>Put maxhp/hp/maxmp/mp/initiative in hero database</li>
@@ -42,7 +44,6 @@ include "checklogin.php";
 	<li>Updated hero search to pull maxhp/maxmp/initiative from database</li>
 	<li>Updated hero list with maxhp/maxmp/initiative</li>
 </ul>
-<hr width='33%'>
 <h2>February 2, 2016</h2>
 <ul>
 	<li>Changed dungeon cooldown to 10 minutes/room from 1 hour/room</li>
@@ -50,7 +51,6 @@ include "checklogin.php";
 	<li>Fixed room counting in dungeons</li>
 	<li>Added the Whiteshire Abbey dungeon</li>
 </ul>
-<hr width='33%'>
 <h2>January 29, 2016</h2>
 <ul>
 	<li>Started recording patch notes</li>
